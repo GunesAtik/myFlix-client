bc92e24105eaf4f8ab27fcefc00c2c1e34568356
„guenes <guenes_atik@hotmail.de>
2021-03-09 22:39:15 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
-
-export default class MainView extends React.Component {
+export class MainView extends React.Component {
 
   constructor() {
-    // Call the superclass constructor
-    // so React can initialize it
     super();
 
-    // Initialize the state to an empty object so we can destructure it later
-    this.state = {};
-  }// One of the "hooks" available in a React Component
+    this.state = {
+      movies: null,
+      selectedMovie: null
+    };
+  }
+
   componentDidMount() {
-    axios.get('https://myflixxx.herokuapp.com/movies')
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    /* ... */
+  }
+
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
   }
 
 
   render() {
-    const { movies } = this.state;
+    const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
     if (!movies) return <div className="main-view" />;
 
     return (
       <div className="main-view">
-        { movies.map(movie => (
-          <MovieCard key={movie._id} movie={movie} />
-        )
-        )
+        {selectedMovie
+          ? <MovieView movie={selectedMovie} />
+          : movies.map(movie => (
+            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
+          ))
         }
       </div>
     );
