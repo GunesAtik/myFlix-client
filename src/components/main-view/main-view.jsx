08a42c637ba1589612b0844ede55ecccdcaecc26
„guenes <guenes_atik@hotmail.de>
2021-03-13 16:40:16 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import axios from 'axios';
-
-import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { RegisterView } from '../registration-view/registration-view'
+
+import './main-view.scss'
+
+import {
+  Navbar,
+  Nav,
+  Container,
+  Row,
+  Col,
+} from 'react-bootstrap';
+
 
 export class MainView extends React.Component {
-
   constructor() {
     super();
-
+    //initial state is set to null
     this.state = {
       movies: null,
       selectedMovie: null,
-      user: null
+      user: null,
+      register: null
     };
   }
 
   componentDidMount() {
-    axios.get('https://myFlixxx.herokuapp.com/movies')
+    axios
+      .get('https://myflixxx.herokuapp.com/movies')
       .then(response => {
-        // Assign the result to the state
         this.setState({
           movies: response.data
         });
@@ -36,9 +47,9 @@ export class MainView extends React.Component {
     });
   }
 
-  onBackClick() {
+  onRegister(register) {
     this.setState({
-      selectedMovie: null
+      register
     });
   }
 
@@ -48,25 +59,67 @@ export class MainView extends React.Component {
     });
   }
 
+  onButtonClick() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
+
   render() {
-    const { movies, selectedMovie, user } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    if (!register) return <RegisterView onRegister={(register) => this.onRegister(register)} />
 
-    // Before the movies have been loaded
-    if (!movies) return <div className="main-view" />;
+    if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+
+    if (!movies) return <div className='main-view'></div>;
 
     return (
-      <div className="main-view">
-        {selectedMovie
-          ? <MovieView movie={selectedMovie}
-            onBackClick={() => this.onBackClick()}
-          />
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
-          ))
-        }
-      </div>
+      <React.Fragment>
+        <div className='main-view'>
+          <header>
+            <Navbar bg='dark' variant='dark'>
+              <Nav className='justify-content-center'>
+                <Nav.Item>
+                  <Nav.Link target='_blank' href='#Home'>Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link target='_blank' href='#Directors'>Directors</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link target='_blank' href='#Genres'>Genres</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link className='logout-button' target='_blank' href='#Home'>Logout</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar>
+          </header>
+          <div className='main-body text-center'>
+            {selectedMovie ? (
+              <MovieView
+                movie={selectedMovie}
+                onClick={() => this.onButtonClick()}
+              />
+            ) : (
+                <Container>
+                  <Row>
+                    {movies.map((movie) => (
+                      <Col xs={12} sm={6} md={4} key={movie._id}>
+                        <MovieCard
+                          key={movie._id}
+                          movie={movie}
+                          onClick={(movie) => this.onMovieClick(movie)}
+                        />
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
+              )}
+          </div>
+          <div className='test'></div>
+        </div>
+      </React.Fragment>
     );
   }
 }
