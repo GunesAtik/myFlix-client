fc42f68e28f5fed53c7bd5ee5aad213131600ab0
„guenes <guenes_atik@hotmail.de>
2021-03-16 19:12:23 +0100

    comment out scss files

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 import { Card, Button } from 'react-bootstrap';
 
-import './movie-view.scss';
+//import './movie-view.scss';
 
 export class MovieView extends React.Component {
 
