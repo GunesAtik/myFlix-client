f8fe6912179b08fdb3aa18346055e5f8c79d8f46
„guenes <guenes_atik@hotmail.de>
2021-04-03 15:11:48 +0200

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,32 +1,28 @@
 import React from 'react';
 import axios from 'axios';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
-import { RegisterView } from '../registration-view/registration-view'
-
-//import './main-view.scss'
-
-import {
-  Navbar,
-  Nav,
-  Container,
-  Row,
-  Col,
-} from 'react-bootstrap';
-
+import { RegistrationView } from '../registration-view/registration-view';
 
 export class MainView extends React.Component {
+
   constructor() {
     super();
-    //initial state is set to null
+
     this.state = {
       movies: null,
-      selectedMovie: null,
-      user: null,
-      register: null
+      user: null
     };
   }
+
+  getMovies() {
+    /* ... */
+  }
+
 
   componentDidMount() {
     let accessToken = localStorage.getItem('token');
@@ -38,105 +34,24 @@ export class MainView extends React.Component {
     }
   }
 
-  onMovieClick(movie) {
-    this.setState({
-      selectedMovie: movie
-    });
-  }
-
-  onRegister(register) {
-    this.setState({
-      register
-    });
-  }
-
   onLoggedIn(authData) {
-    console.log(authData);
-    this.setState({
-      user: authData.user.Username
-    });
-
-    localStorage.setItem('token', authData.token);
-    localStorage.setItem('user', authData.user.Username);
-    this.getMovies(authData.token);
-  }
-
-  onButtonClick() {
-    this.setState({
-      selectedMovie: null
-    });
-  }
-
-  getMovies(token) {
-    axios.get('https://myflixxx.herokuapp.com/movies', {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-      .then(response => {
-        // Assign the result to the state
-        this.setState({
-          movies: response.data
-        });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    ...
   }
 
   render() {
-    const { movies, selectedMovie, user, register } = this.state;
+    const { movies, user } = this.state;
 
-    if (!register) return <RegisterView onRegister={(register) => this.onRegister(register)} />
 
-    if (!user) return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
-
-    if (!movies) return <div className='main-view'></div>;
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+    if (!movies) return <div className="main-view" />;
 
     return (
-      <React.Fragment>
-        <div className='main-view'>
-          <header>
-            <Navbar bg='dark' variant='dark'>
-              <Nav className='justify-content-center'>
-                <Nav.Item>
-                  <Nav.Link target='_blank' href='#Home'>Home</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link target='_blank' href='#Directors'>Directors</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link target='_blank' href='#Genres'>Genres</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link className='logout-button' target='_blank' href='#Home'>Logout</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Navbar>
-          </header>
-          <div className='main-body text-center'>
-            {selectedMovie ? (
-              <MovieView
-                movie={selectedMovie}
-                onClick={() => this.onButtonClick()}
-              />
-            ) : (
-                <Container>
-                  <Row>
-                    {movies.map((movie) => (
-                      <Col xs={12} sm={6} md={4} key={movie._id}>
-                        <MovieCard
-                          key={movie._id}
-                          movie={movie}
-                          onClick={(movie) => this.onMovieClick(movie)}
-                        />
-                      </Col>
-                    ))}
-                  </Row>
-                </Container>
-              )}
-          </div>
-          <div className='test'></div>
+      <Router>
+        <div className="main-view">
+          <Route exact path="/" render={() => movies.map(m => <MovieCard key={m._id} movie={m} />)} />
+          <Route path="/movies/:movieId" render={({ match }) => <MovieView movie={movies.find(m => m._id === match.params.movieId)} />} />
         </div>
-      </React.Fragment>
+      </Router>
     );
   }
 }
